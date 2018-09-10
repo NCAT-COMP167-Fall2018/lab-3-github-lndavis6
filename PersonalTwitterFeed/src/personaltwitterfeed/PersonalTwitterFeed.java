175de5ef11b29878adbc3395cc599dce498e5031
Lance Nathaniel Davis <lndavis6@ncat.edu>
2018-09-10 16:04:56 -0400

    feature 2

--- PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
+++ PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
@@ -15,18 +15,20 @@ import java.util.Scanner;
  */
 public class PersonalTwitterFeed {
 
-    private static int MAX_NUMBER_TWEETS = 200;
+    private static final int MAX_NUMBER_TWEETS = 200;
+    private static String Scanner keyboard;
+    private static String tweeterName;
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         String[] tweets = new String[MAX_NUMBER_TWEETS];
-        Scanner keyboard = new Scanner(System.in);
+        keyboard = new Scanner(System.in);
         System.out.println("Welcome to your personal Twitter!");
         System.out.println("What's your name, tweeter?");
         
-        String tweeterName = keyboard.nextLine();
+        tweeterName = keyboard.nextLine();
         
         System.out.println("Nice to meet you " + tweeterName + "!");
         System.out.println("Enter your tweets and I will add them to your timeline!");
@@ -54,10 +56,32 @@ public class PersonalTwitterFeed {
         System.out.println("Your twitter feed is full");
     }
     public static String getCurrentTimeStamp(){
-        String pattern = "YYYY-MM-dd HH:mm:ss";
+        String pattern = "yyyy-MM-dd HH:mm:ss.SSSZ";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
         String time = simpleDateFormat.format(new Date());
         return time;
     }
-    
+    public static void newTweet(String [] tweets){
+        int numTweets = 0;
+        
+        while(numTweets < (MAX_NUMBER_TWEETS - 1)) {
+            tweets[numTweets] = keyboard.nextLine();
+            numTweets++;
+            
+            System.out.println(tweeterName + "'s Personal Twitter Feed:");
+            for(int i = 0; i < numTweets; i++) {
+                System.out.println("- " + tweets[i] + " at " + getCurrentTimeStamp());
+            }
+            
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            
+            if(numTweets < (MAX_NUMBER_TWEETS - 1))
+                System.out.println("Enter your next tweet:");
+        }
+        
+        System.out.println("Your twitter feed is full");
 }
+    }
